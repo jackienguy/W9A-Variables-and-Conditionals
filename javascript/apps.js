2fddb9ef0dcc622a19fa0c31d1c2f1c86de536fd
Your Name <you@example.com>
2021-07-08 23:11:21 -0600

    moving conditional blck into the body and removing else if

--- javascript/apps.js
+++ javascript/apps.js
@@ -5,18 +5,18 @@ for (var counter = 0; counter < 8; counter++) {
     console.log(userAge[counter]);
     console.log(is_subscribed[counter]);
     console.log();
+    if (userAge[counter] < 18) {
+        if (is_subscribed[counter] != true) {    
+            console.log("The user is younger than 18 and not subscribed");
+        } else if (is_subscribed[counter]) {
+            console.log("The user is younger than 18 and is subscribed");
+        }
+    } else  {
+        if (is_subscribed[counter] != true) {
+            console.log("The user is 18 or older and not subscribed");
+        } else if (is_subscribed[counter]) {
+            console.log("The user is 18 or older and is subscribed");
+        }
+    }
 }
 
-if (userAge < 18) {
-    if (is_subscribed != true) {    
-        console.log("The user is younger than 18 and not subscribed");
-    } else if (is_subscribed) {
-        console.log("The user is younger than 18 and is subscribed");
-    }
-} else if (userAge >= 18) {
-    if (is_subscribed != true) {
-        console.log("The user is 18 or older and not subscribed");
-    } else if (is_subscribed) {
-        console.log("The user is 18 or older and is subscribed");
-    }
-}
